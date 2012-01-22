8583ef81cca7a94f93b7b9fae01bf7e134d49525
Matthijs van Henten <matthijs@ischen.nl>
2012-01-22 23:06:48 +0100

    added selectors and some hacks for transmediale

--- js/glue.js
+++ js/glue.js
@@ -1,6 +1,23 @@
-"use strict";
+//"use strict";
 var glue_me = ( function () {
-    var selectors = 'img,a:visible,p,h1,.festival-col, .view-content, #sidebar-right';
+    /**
+     * Hand-picked selectors from transmediale
+     * 
+     */
+    var selectors = ['p,img,a:visible,h1:visible,h2,h3',
+        'div.cloud',
+        'div.views-row',
+        'div.festival-menu',
+        'div.festivalmenuhead',
+        'div.festival-col',
+        'div.field',
+        '#block-block-219',
+        '.blue',
+        '.goldheader',
+        '.goldtext',
+        '#sidebar-right .content',
+        '#sidebar-right .block',
+    ].join(',');
 
     $('*').contents().each(function() {
         if(this.nodeType == 8) {
@@ -26,6 +43,7 @@ var glue_me = ( function () {
             obj.style.top  = offset.top + 'px';
             obj.style.left = offset.left + 'px';
             obj.properties = {};
+            var collect = true; // quick hack see below
 
             switch( obj.element.tagName.toLowerCase() ){
                 case 'img':
@@ -38,40 +56,74 @@ var glue_me = ( function () {
                     obj.type = 'link';
                     obj.text = $(obj.element).text();
                     break;
-                default:
+                case 'div':
+                    /**
+                     * ok some hacks here
+                     *
+                     * - filter out divs that have background images
+                     * - fiter out divs that have background colors
+                     * - all other stuff is not interesting.
+                     */
                     if( obj.style['background-image'] != 'none' ){
-                        try {
-                            var src = obj.style['background-image'];
-                            console.log(src);
-                            src = /url\((.+?)\)/.exec(src)[1];
+                        var src = obj.style['background-image'];
+                        src = /url\((.+?)\)/.exec(src);
 
+                        if( src && src.length === 2 ){
                             var objCopy = {
                                 type: 'image',
                                 text: '',
-                                style: obj.style,
+                                style: obj.style, //NB need to do something with z-index
                                 properties: {
-                                    src: src
+                                    src: src[1]
                                 }
-                            }
-
-                            collect.push(objCopy);
+                            };
+                            collector.push(objCopy);
                         }
-                        catch(e){
-                            console.log(e);
-                        }
+                        var clone = $(obj.element).clone();
+                        $(clone).find('a,img,p,h1,h2,h3,h4').remove();
+                        obj.text = $(clone).html();
+                        obj.type = 'text';
+                    }
+                    else if( obj.style['background-color'] != 'rgba(0, 0, 0, 0)' ){
+                        var clone = $(obj.element).clone();
+                        $(clone).find('a,img,p,h1,h2,h3,h4').remove();
+                        obj.text = $(clone).html();
+                        obj.type = 'text';
+                    }
+                    else{
+                        collect = false;
                     }
 
-                    //$(obj.element).find('*').inlineCSS();
+                    break;
+                case 'h1':
+                    /**
+                     * WARNING this is a huge hack to work around -webkit- background
+                     * images, wich obfuscate the background image fallback
+                     * given in this instance. It simply assumes all H1 tags on the
+                     * transmediale site carry this background!
+                     */
+                    var objCopy = {
+                        type: 'image',
+                        text: '',
+                        style: obj.style, //NB need to do something with z-index
+                        properties: {
+                            src: 'http://www.transmediale.de/sites/www.transmediale.de/themes/ninesixty/styles/tm12/webfiles/img12/titlebg.png'
+                        }
+                    };
+                    collector.push(objCopy);
+                    // NO BREAK intentionally!
+                default:
                     var clone = $(obj.element).clone();
-
-                    $(clone).find('a,img,p').remove();
+                    $(clone).find('a,img,p,h1,h2,h3,h4').remove();
                     obj.text = $(clone).html();
                     obj.type = 'text';
                     break;
             }
 
-            delete(obj.element);
-            collector.push(obj);
+            if( collect ){
+                delete(obj.element);
+                collector.push(obj);
+            }
         },
 
         collect: function(){

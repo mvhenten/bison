5dc19592a696d33ef1f70a0ea6f5b1327eb79e67
Matthijs van Henten <matthijs@ischen.nl>
2012-01-23 21:58:00 +0100

    fixed a huge namespace bug in util

--- Glue/Util.php
+++ Glue/Util.php
@@ -7,109 +7,115 @@
  */
 
 
-namespace Glue\Util;
+namespace Glue\Util {
+    abstract class Base {
 
-abstract class Base {
+        /**
+         *
+         *
+         * @param unknown $name
+         * @return unknown
+         */
+        public function __get( $name ) {
+            if ( ($property = "_$name")
+                && (isset($this->$property) || property_exists( $this, $property ))) {
+                if ( ! isset( $this->$property )
+                    && method_exists( $this, "_build_$property" ) ) {
+                    $method = "_build_$property";
+                    $this->$property = $this->$method();
+                }
+                return $this->$property;
+            }
+        }
+
+
+    }
+
+
+}
+
+
+namespace Glue {
+
 
     /**
      *
      *
-     * @param unknown $name
+     * @param unknown $function
+     * @param unknown $array
      * @return unknown
      */
-    public function __get( $name ) {
-        if ( ($property = "_$name")
-            && (isset($this->$property) || property_exists( $this, $property ))) {
-            if ( ! isset( $this->$property )
-                && method_exists( $this, "_build_$property" ) ) {
-                $method = "_build_$property";
-                $this->$property = $this->$method();
-            }
-            return $this->$property;
+    function map( $function, $array ) {
+        return array_map( $function, array_keys($array), array_values($array) );
+    }
+
+
+    /**
+     *
+     *
+     * @param array   $array
+     * @param unknown $keys
+     * @return unknown
+     */
+    function slice( array $array, $keys ) {
+        $args  = func_get_args();
+        $input = array_shift($args);
+
+        if ( is_array( $args[0] ) ) {
+            $args = $args[0];
         }
+
+        $args = array_flip($args);
+
+        return array_intersect_key( $input, $args );
     }
 
 
-}
-
-
-/**
- * Glue/Util.php
- *
- * @author Matthijs van Henten <matthijs@ischen.nl>
- * @package Bison
- * @param unknown $function
- * @param unknown $array
- * @return unknown
- */
-function map( $function, $array ) {
-    return array_map( $function, array_keys($array), array_values($array) );
-}
-
-
-/**
- *
- *
- * @param array   $array
- * @param unknown $keys
- * @return unknown
- */
-function slice( array $array, $keys ) {
-    $args  = func_get_args();
-    $input = array_shift($args);
-
-    if ( is_array( $args[0] ) ) {
-        $args = $args[0];
+    /**
+     *
+     *
+     * @param unknown $str
+     * @return unknown
+     */
+    function qw( $str ) {
+        return array_filter( array_map('trim', preg_split( '/\s/', trim($str) ) ) );
     }
 
-    $args = array_flip($args);
 
-    return array_intersect_key( $input, $args );
-}
+    /**
+     *
+     *
+     * @param unknown $args
+     * @return unknown
+     */
+    function optparse( $args ) {
+        $options = func_get_args();
 
+        $longopts = array();
+        $shortopt = '';
 
-/**
- *
- *
- * @param unknown $str
- * @return unknown
- */
-function qw( $str ) {
-    return array_filter( array_map('trim', preg_split( '/\s/', trim($str) ) ) );
-}
-
-
-/**
- *
- *
- * @param unknown $args
- * @return unknown
- */
-function optparse( $args ) {
-    $options = func_get_args();
-
-    $longopts = array();
-    $shortopt = '';
-
-    $keys   = qw('long short required switch default help');
-    $lookup = array();
+        $keys   = qw('long short required switch default help');
+        $lookup = array();
 
 
-    foreach ( $options as $option ) {
-        @list( $long, $short, $required, $switch, $default, $help ) = slice( $option, $keys );
+        foreach ( $options as $option ) {
+            @list( $long, $short, $required, $switch, $default, $help ) = slice( $option, $keys );
 
-        if ( $required ) {
-            $shortopts .= "$short:";
-            $longopts[] = "$long:";
-        }
-        else if ( !$switch ) {
-                $shortopts .= "$short::";
-                $longopts[] = "$long::";
+            if ( $required ) {
+                $shortopts .= "$short:";
+                $longopts[] = "$long:";
             }
+            else if ( !$switch ) {
+                    $shortopts .= "$short::";
+                    $longopts[] = "$long::";
+                }
 
-        $lookup[$short] = $option;
-        $lookup[$long]  = $option;
+            $lookup[$short] = $option;
+            $lookup[$long]  = $option;
+        }
+
+        return getopt( $opts );
     }
 
-    return getopt( $opts );
+
 }

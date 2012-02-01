e90026b452c5b59a66ee2ee978d188ce6b59bd97
Matthijs van Henten <mvhenten@tty.nl>
2012-02-01 15:13:32 +0100

    path

--- www/janitor.php
+++ www/janitor.php
@@ -5,8 +5,8 @@
 ini_set('display_errors', "false");
 ini_set('display_warnings', "false");
 
-define( 'BISON_WWW_PATH', '/home/matthijs/tmp' );
-//define( 'BISON_WWW_PATH', '/home/hotglue/www-transglue' );
+//define( 'BISON_WWW_PATH', '/home/matthijs/tmp' );
+define( 'BISON_WWW_PATH', '/home/hotglue/www-transglue' );
 define( 'BISON_PAGE_LIST_CACHE', sys_get_temp_dir() . '/bison-page-list.json');
 
 define( 'BISON_INSTANCE_MAX_AGE', '-3 hours' );
